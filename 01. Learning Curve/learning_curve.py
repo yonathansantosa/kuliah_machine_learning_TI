31bea5b0e8cc942185d46aa2d41947beac372d86
yonathansantosa <yonathansantosa@gmail.com>
2022-09-21 07:04:19 +0700

    init

---
+++ 01. Learning Curve/learning_curve.py
@@ -0,0 +1,35 @@
+
+def plot_training_curve(clf, X_train, y_train):
+    train_sizes, train_scores, test_scores = learning_curve(clf, X_train, y_train, scoring="accuracy")
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    fig, axes = plt.subplots(1,1,figsize=(10, 15))
+
+    # Plot learning curve
+    axes.grid()
+    axes.fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="r",
+    )
+    axes.fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="g",
+    )
+    axes.plot(
+        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    )
+    axes.plot(
+        train_sizes, test_scores_mean, "o-", color="g", label="Test score"
+    )
+    axes.legend(loc="best")
+
+    plt.show()
